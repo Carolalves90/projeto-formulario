9be2e22b5d0b4732a24433dc80b8e2b323a1ef5d
Carolalves90 <carolalves.tech@gmail.com>
2023-05-26 18:26:41 -0300

    :sparkles: validando formulario

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import '@fontsource/roboto'
 
-import { Container } from '@material-ui/core';
+import { Container, Typography  } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
       <Container component='article' maxWidth='sm'>
-        <h1>Formulário de cadastro</h1>
-        <FormularioCadastro />
+        <Typography variant='h3' component='h1' align='center' >Formulário de Cadastro</Typography >
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
       </Container>
     )
+  }
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados)
+}
+
+function validarCPF(cpf) {
+  if(cpf.length !== 11){
+    return { valido: false, texto: "CPF deve conter 11 dígitos" }
+  }else{
+    return { valido: true, texto: " " }
   }
 }
 
